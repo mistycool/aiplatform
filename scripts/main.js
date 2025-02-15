9826e51506260cdd70724b1f373c4460dd5def3c
mistycool <mistycool666@gmail.com>
2025-02-15 19:59:16 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,3 @@
-function openMenu() {
-  const menu = document.querySelector('.menu');
-  menu.style.left = '0'; // Slide in on hover
-}
-
-// Optional: Close menu when clicking outside
-document.addEventListener('click', (event) => {
-  const menu = document.querySelector('.menu');
-  const menuIcon = document.querySelector('.menu-icon');
-  if (!menu.contains(event.target) && !menuIcon.contains(event.target)) {
-    menu.style.left = '-250px'; // Slide out when clicking outside
-  }
-});
 // Toggle Menu on Click
 document.querySelector('.menu-icon').addEventListener('click', function () {
   const menu = document.querySelector('.menu');
